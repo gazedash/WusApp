6bdb7a01d4254744d98878ad294fc8a3bf8c90b9
Andrey <andrey@Andreys-MacBook-Pro.local>
2019-03-26 19:56:38 +0300

    Fix

--- app/src/main/java/com/example/myapplicationplayer/MainActivity.java
+++ app/src/main/java/com/example/myapplicationplayer/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.myapplicationplayer;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.session.MediaController;
+import android.media.session.MediaSession;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -37,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
         time = sharedPref.getInt("time", 10);
         et = findViewById(R.id.edittext);
         et.setText(url);
+
+//        MediaController.
+
+//        new MediaSession(MainActivity.this, "blblblb ").getController().dispatchMediaButtonEvent(keyEvent);
 
         np = findViewById(R.id.numberPicker);
         np.setMaxValue(23);
@@ -72,14 +85,15 @@ public class MainActivity extends AppCompatActivity {
         
         calendar.setTime(new Date());
 
-        calendar.set(Calendar.HOUR_OF_DAY, finalTime);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
+//        calendar.set(Calendar.HOUR_OF_DAY, finalTime);
+//        calendar.set(Calendar.MINUTE, 0);
+//        calendar.set(Calendar.SECOND, 0);
 
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 234324243, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY, pendingIntent);
+//        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis() + 3000, pendingIntent);
         sharedPref.edit().putString("url", txt).commit();
         sharedPref.edit().putInt("time", finalTime).commit();
         Toast.makeText(this, "Alarm set in " + finalTime + " hours",
--- app/src/main/java/com/example/myapplicationplayer/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplicationplayer/MyBroadcastReceiver.java
@@ -3,20 +3,51 @@ package com.example.myapplicationplayer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.net.Uri;
+import android.os.SystemClock;
+import android.view.KeyEvent;
+import android.view.WindowManager;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         //start activity
-        String url = intent.getExtras().getString("url");
+        String url = intent.getExtras().getString("url", "");
+
+        AudioManager am =
+                (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+//
+        am.setStreamVolume(
+                AudioManager.STREAM_MUSIC,
+                am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
+                0);
+
 //        Intent i = new Intent();
 //        i.setClassName("com.example.myapplicationplayer", "com.example.myapplicationplayer.MainActivity");
         //"https://open.spotify.com/track/6oBjnOBqngXWsRuYY5eTnz?si=BaICUmnzTj2Yarx8NNvpEw"));
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         browserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        browserIntent.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED +
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD +
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON +
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         context.startActivity(browserIntent);
+
+
+        AudioManager mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        long eventtime = SystemClock.uptimeMillis();
+        Intent keyIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent keyEvent = new KeyEvent(eventtime, eventtime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY, 0);
+        keyIntent.putExtra(Intent.EXTRA_KEY_EVENT, keyEvent);
+        mAudioManager.dispatchMediaKeyEvent(keyEvent);
+
+        keyEvent = KeyEvent.changeAction(keyEvent, KeyEvent.ACTION_UP);
+        keyIntent.putExtra(Intent.EXTRA_KEY_EVENT, keyEvent);
+
+        mAudioManager.dispatchMediaKeyEvent(keyEvent);
     }
 }
 
